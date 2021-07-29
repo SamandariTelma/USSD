1e0198d91c3dd6e83293d4118de92979c81cb211
samandari <samandari@D2019Q3FFK.corp.telma.mg>
2021-07-29 11:34:39 +0300

    Ajout err de confirmation

---
+++ Scripts/Promotion/04-YELOW/01-Menu YELOW/Achat YELOW erreur de confirmation/Script1627545864929.groovy
@@ -0,0 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+String numeroInitiateur="${numeroInitiateur}"
+
+'Je me rends sur le menu promotion YELOW en shortCodant *130*5*3#'
+CustomKeywords.'ussd.Send.code'(GlobalVariable.shortCode+'*4#', numeroInitiateur)
+
+'Je saisis un menu offre YELOW'
+CustomKeywords.'ussd.Send.response'('2')
+
+'Dans le prompt de confirmation, je saisi un chiffre inexistant'
+String actualMenu=CustomKeywords.'ussd.Send.response'('3')
+
+'Vérifier la conformité du message'
+String menu=CustomKeywords.'ussd.Expected.menu'('Cette option n\'est pas permise\\.', 'Safidy tsy manankery\\.')
+
+WS.verifyMatch(actualMenu, menu, true)
