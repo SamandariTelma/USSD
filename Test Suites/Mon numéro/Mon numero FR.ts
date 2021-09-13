0dca7217c4be31ae73d8534bb1e0c5f4520c0720
samandari <samandari@D2019Q3FFK.corp.telma.mg>
2021-09-13 11:25:28 +0300

    Menu mon numéro

---
+++ Test Suites/Mon numéro/Mon numero FR.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Mon numero FR</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>efc35cf5-8df4-471b-a9d5-e1c82166a163</testSuiteGuid>
+   <testCaseLink>
+      <guid>99552dec-65b7-49a5-9a78-3201d7d9e72d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Services TELMA/Mon numéro/Consulter menu mon numero</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c3775c1c-99d9-4e83-8c45-60e5117eeca6</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c628203f-7238-4a03-8b2c-86b78e9dde53</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Services TELMA/Mon numéro/Consulter mon numero via service telma</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b5bc0b77-2542-4e2c-b72a-08941cf03e3f</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6e8d4ec9-d3f4-4ffc-a59c-e422f4de4958</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Called Test Case/Changer de langue</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c3c6a709-03d4-49f5-85a3-4205a8dc8a53</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
