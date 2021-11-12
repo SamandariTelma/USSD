ab28184792bcaff3584a5dcaebd485beca448b17
samandari <samandari@D2019Q3FFK.corp.telma.mg>
2021-11-12 17:23:03 +0300

    finalisation envoyer stock

---
+++ Scripts/2TMV/Envoyer Stock/09 - Envoi Stock - Point de vente/04 - Point de vente - Chef de zone/Script1636705952463.groovy
@@ -0,0 +1,28 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+String numeroEnvoyeur="${numeroEnvoyeur}"
+String numeroRecepteur="${numeroRecepteur}"
+String montantStock="${montantStock}"
+
+'En tant que numero de type Revendeur, j\'envoi du stock à un numéro de type Chef de zone'
+WebUI.callTestCase(findTestCase('2TMV/Envoyer Stock/00-Called test case/Envoi stock - destinataire non eligible'), [('numeroEnvoyeur') : numeroEnvoyeur
+        , ('numeroRecepteur') : numeroRecepteur, ('numeroANotifier') : GlobalVariable.msisdnPourNotification
+        , ('pinEnvoyeur') : GlobalVariable.pinEnvoyeur, ('montantStock') : montantStock], FailureHandling.CONTINUE_ON_FAILURE)
+
