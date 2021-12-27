4a0e0f37285094389d5dd9f8a5f67b6b868c0437
samandari <samandari@D2019Q3FFK.corp.telma.mg>
2021-12-27 17:27:39 +0300

    SC Achat Mora pour autre

---
+++ Scripts/Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY08 - Achat YELOW 100 - Limite d achat/Script1640588866921.groovy
@@ -0,0 +1,66 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+String numeroInitiateur = "$numeroInitiateur"
+
+'2e tentative d\'achat yelow 100'
+WebUI.callTestCase(findTestCase('Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY01 - Achat YELOW 100'), 
+    [('numeroInitiateur') : numeroInitiateur], FailureHandling.CONTINUE_ON_FAILURE)
+
+'3e tentative d\'achat yelow 100'
+WebUI.callTestCase(findTestCase('Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY01 - Achat YELOW 100'),
+	[('numeroInitiateur') : numeroInitiateur], FailureHandling.CONTINUE_ON_FAILURE)
+
+'4e tentative d\'achat yelow 100'
+WebUI.callTestCase(findTestCase('Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY01 - Achat YELOW 100'),
+	[('numeroInitiateur') : numeroInitiateur], FailureHandling.CONTINUE_ON_FAILURE)
+
+'5e tentative d\'achat yelow 100'
+WebUI.callTestCase(findTestCase('Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY01 - Achat YELOW 100'),
+	[('numeroInitiateur') : numeroInitiateur], FailureHandling.CONTINUE_ON_FAILURE)
+
+'6e tentative d\'achat yelow 100'
+WebUI.callTestCase(findTestCase('Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY01 - Achat YELOW 100'),
+	[('numeroInitiateur') : numeroInitiateur], FailureHandling.CONTINUE_ON_FAILURE)
+
+'7e tentative d\'achat yelow 100'
+WebUI.callTestCase(findTestCase('Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY01 - Achat YELOW 100'),
+	[('numeroInitiateur') : numeroInitiateur], FailureHandling.CONTINUE_ON_FAILURE)
+
+'8e tentative d\'achat yelow 100'
+WebUI.callTestCase(findTestCase('Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY01 - Achat YELOW 100'),
+	[('numeroInitiateur') : numeroInitiateur], FailureHandling.CONTINUE_ON_FAILURE)
+
+'9e tentative d\'achat yelow 100'
+WebUI.callTestCase(findTestCase('Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY01 - Achat YELOW 100'),
+	[('numeroInitiateur') : numeroInitiateur], FailureHandling.CONTINUE_ON_FAILURE)
+
+'10e tentative d\'achat yelow 100'
+WebUI.callTestCase(findTestCase('Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY01 - Achat YELOW 100'),
+	[('numeroInitiateur') : numeroInitiateur], FailureHandling.CONTINUE_ON_FAILURE)
+
+'11e tentative Je tente d\'acheter un offre YELOW100 :  *611*67#'
+String actualMenu = CustomKeywords.'ussd.Send.code'(GlobalVariable.shortCodeDirectAchat + '*67#', numeroInitiateur)
+
+'Vérifier la conformité du message'
+String menu = CustomKeywords.'ussd.Expected.menu'('Desole, vous avez utilise toutes vos demandes pour aujourd\'hui\\. Vous pourrez envoyer 10 demande\\(s\\) demain\\.', 
+    'Tapitra ny fahafahanao mampiasa io tolotra io androany\\. Rahampitso indray ianao afaka mividy  10\\.')
+
+WS.verifyMatch(actualMenu, menu, true)
+
---
+++ Scripts/Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY10 - Achat YELOW Facebobaka - Limite d achat/Script1640589045125.groovy
@@ -0,0 +1,36 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+String numeroInitiateur = "$numeroInitiateur"
+
+String nextDate=CustomKeywords.'ussd.Util.nextDate'(3)
+
+'2eme tentative d achat yelow facebobaka'
+WebUI.callTestCase(findTestCase('Services TELMA/Changer de tarif/04-YELOW/SHORTCODE 611 et 655/Achat offre (611)/SCAOY03 - Achat YELOW Facebobaka'), 
+    [('numeroInitiateur') : numeroInitiateur], FailureHandling.CONTINUE_ON_FAILURE)
+
+'Je tente d ahcheter un offre YELOW Facebobaka pour la 3ème fois :  *611*65#'
+String actualMenu = CustomKeywords.'ussd.Send.code'(GlobalVariable.shortCodeDirectAchat + '*65#', numeroInitiateur)
+
+'Vérifier la conformité du message'
+String menu = CustomKeywords.'ussd.Expected.menu'(('Desole, vous avez utilise toutes vos demandes\\. Vous pourrez envoyer 2 demandes a partir du ' + 
+    nextDate) + '\\.', ('Tapitra ny fahafahanao mampiasa io tolotra io\\. Amin ny ' + nextDate) + ' indray ianao afaka mividy 2\\.')
+
+WS.verifyMatch(actualMenu, menu, true)
+
