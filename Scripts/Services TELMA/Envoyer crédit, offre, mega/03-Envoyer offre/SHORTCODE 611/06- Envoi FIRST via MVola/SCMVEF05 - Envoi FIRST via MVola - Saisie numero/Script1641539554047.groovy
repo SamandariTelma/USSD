26af9740d935d20baecdee9387e1019e8a965393
samandari <samandari@D2019Q3FFK.corp.telma.mg>
2022-01-07 17:04:21 +0300

    SC Telma Net MVola

---
+++ Scripts/Services TELMA/Envoyer crédit, offre, mega/03-Envoyer offre/SHORTCODE 611/06- Envoi FIRST via MVola/SCMVEF05 - Envoi FIRST via MVola - Saisie numero/Script1641539554047.groovy
@@ -0,0 +1,41 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+String numeroInitiateur="${numeroInitiateur}"
+String numeroRecepteur="${numeroRecepteur}"
+String montant="${montant}"
+numeroRecepteur=CustomKeywords.'ussd.Util.to034'(numeroRecepteur)
+
+
+'En tant que GP, j\'effectue un envoi offre First Royal via MVola avec un numéro non Telma:  *611*95*1*numeroNonTelma#'
+String actualMenu=CustomKeywords.'ussd.Send.code'(GlobalVariable.shortCodeDirectAchat+'*95*1*0325785400#', numeroInitiateur)
+
+'Vérifier la conformité du message'
+String menu = CustomKeywords.'ussd.Expected.menu'('Desole, vous ne pouvez pas utiliser ce service\\.',
+	'Azafady, tsy afaka mampiasa an\'io servisy io ianao\\.')
+WS.verifyMatch(actualMenu, menu, true)
+
+'En tant que GP, j\'effectue un envoi offre First Royal via MVola à moi même:  *611*95*1*monNumero#'
+actualMenu=CustomKeywords.'ussd.Send.code'(GlobalVariable.shortCodeDirectAchat+'*95*1*'+numeroRecepteur+'#', numeroInitiateur)
+
+'Vérifier la conformité du message'
+menu = CustomKeywords.'ussd.Expected.menu'('Vous ne pouvez pas transferer de credit a votre numero\\. Merci de saisir un autre numero TELMA Mobile\\.',
+	'Tsy afaka mamindra fahana mankany amin ny nomeraonao ianao\\. Mampidira nomerao TELMA Mobile hafa\\.')
+
+WS.verifyMatch(actualMenu, menu, true)
