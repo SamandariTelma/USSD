bf1f7cb8e696453213824665ac1480cb659dad25
samandari <samandari@D2019Q3FFK.corp.telma.mg>
2022-01-18 16:56:29 +0300

    envoi Net via MVola

---
+++ Scripts/Services TELMA/Envoyer crédit, offre, mega/03-Envoyer offre/SHORTCODE 611/08- Envoi TELMA NET via MVola/SCMVET10 - Envoi via MVola TELMA NET - Saisie numéro/Script1642512255367.groovy
@@ -0,0 +1,28 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+String numeroInitiateur="${numeroInitiateur}"
+
+'En tant que GP, j\'effectue un envoi offre NET One Month 100 Go via MVola avec un numéro invalide:  *611*15*1*numeroInvalide#'
+String actualMenu=CustomKeywords.'ussd.Send.code'(GlobalVariable.shortCodeDirectAchat+'*15*2*0325785400#', numeroInitiateur)
+
+'Vérifier la conformité du message'
+String menu = CustomKeywords.'ussd.Expected.menu'('Desole, vous ne pouvez pas utiliser ce service\\.',
+	'Azafady, tsy afaka mampiasa an\'io servisy io ianao\\.')
+WS.verifyMatch(actualMenu, menu, true)
